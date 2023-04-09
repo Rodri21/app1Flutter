2d67594bb953be1ae28f27826eb8217b732ebe62
Rodrigo Salazar <salazarrodrigo@gmail.com>
2023-04-09 11:36:12 -0600

    commit

--- lib/screens/eventos_screen.dart
+++ lib/screens/eventos_screen.dart
@@ -1,5 +1,13 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
+import 'package:programa1/database/database_helper.dart';
+import 'package:programa1/models/event_model.dart';
 import 'package:programa1/screens/list_event.dart';
+import 'package:programa1/widgets/menu_widget.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
+
 
 class Events extends StatefulWidget {
   const Events({super.key});
@@ -9,42 +17,109 @@ class Events extends StatefulWidget {
 }
 
 class _EventsState extends State<Events> {
+
+  DateTime selectedDay = DateTime.now();
+  Map<DateTime,List<EventModel>> selectedEvents = {};
+  final TextEditingController _eventController = TextEditingController();
+  final DatabaseHelper database = DatabaseHelper();
+  EventModel? objEventModel;
+
+  void _onDaySelected(DateTime day, DateTime focusedDay){
+    setState(() {
+      selectedDay = day;
+    });
+  }
+
+  CalendarStyle _calendarStyle(){
+    return const CalendarStyle(
+      isTodayHighlighted: true,
+    );
+  }
+
+  List<EventModel> _getEventsFromDay(DateTime date){
+    return selectedEvents[date] ?? [];
+  }
+
+  String stringDate(DateTime date){
+    String formattedDate = DateFormat('dd/MM/yyyy').format(date);
+    return formattedDate;
+  }
+
+  @override
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    _eventController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset : false,
       appBar: AppBar(
         title: const Text('Social TEC :)'),
       ),
-      body: ListEvent(),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 330,
+            child: TableCalendar( 
+              rowHeight: 40,
+              headerStyle: const HeaderStyle(formatButtonVisible: false, titleCentered: true), 
+              availableGestures: AvailableGestures.all,
+              selectedDayPredicate: (date)=>isSameDay(date, selectedDay),
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              onDaySelected: _onDaySelected,
+              focusedDay: selectedDay,
+              eventLoader: _getEventsFromDay,
+              calendarStyle: _calendarStyle(),
+            ),
+          ),
+          Expanded(child: SingleChildScrollView(
+            padding: const EdgeInsets.only(bottom: 70),
+            child: ListEvent(date: selectedDay),
+          )),
+        ],
+      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          Navigator.pushNamed(context, '/addEvent').then((value) => {
-            setState((){})
-          });
+          showDialog(context: context, builder: (context) => AlertDialog(
+            title: const Text('Add event'),
+            content: TextFormField(controller: _eventController,),
+            actions: [
+              TextButton(onPressed: () {
+                Navigator.pop(context);
+              }, child: const Text('Cancel')),
+              TextButton(onPressed: () {
+                database.INSERT('tblEvent',{
+                  'dscEvento' : _eventController.text,
+                  'fechaEvento' : selectedDay.toIso8601String(),
+                  'completado' : 0,
+                }).then((value){
+                  var msg = value > 0 
+                    ? 'Evento agregado' 
+                    : 'Ocurrió un error';
+                  var snackBar = SnackBar(
+                    content: Text(msg)
+                  );
+                  Navigator.pop(context);
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  setState(() {});
+                });
+              }, child: const Text('Ok')),
+            ],
+          ));
         }, 
         label: const Text('Add event!'),
-        icon: const Icon(Icons.add_comment),
+        icon: const Icon(Icons.add),
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            const UserAccountsDrawerHeader(
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://previews.123rf.com/images/dclipart/dclipart1612/dclipart161200025/69075752-logotipo-de-la-mascota-del-lince-jefe-de-lince-aislado-en-blanco-ilustraci%C3%B3n-vectorial.jpg'),
-                ),
-                accountName: Text('Rodrigo Salazar'),
-                accountEmail: Text('rodrigo@gmial.com')),
-            ListTile(
-              onTap: () {},
-              title: const Text('Practica 1'),
-              subtitle: const Text('Descripcion de la practica'),
-              leading: const Icon(Icons.settings),
-              trailing: const Icon(Icons.chevron_right),
-            )
-          ],
-        ),
-      ),
+      drawer: const MenuWidget(),
     );
   }
 }
